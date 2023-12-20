f020884f4a0fbb7fde932351a78a0547fcdb6e9c
Hemant Sharma <hs8112002@gmail.com>
2023-12-20 04:11:06 -0500

    Update script.js

--- Count Words and Count Charaters/script.js
+++ Count Words and Count Charaters/script.js
@@ -1,1 +1,32 @@
+class Count{
+    constructor(){
+        this.textArea=document.querySelector('.count_textarea')
+        this.wordCount=document.querySelectorAll('.word-count')
+        this.charCount=document.querySelectorAll('.character-count')
+        window.addEventListener('load',this.updateCount.bind(this))
+        this.textArea.addEventListener('input',this.updateCount.bind(this))
+    }
+    countWords(){
+        const value=this.textArea.value.trim()
+        if(!value) return 0
+        return value.split(/\s+/).length
+    }
+    countChars(){
+        return this.textArea.value.length
+    }
 
+    updateCount(){
+        const numWords=this.countWords()
+        const numChars=this.countChars()
+
+        this.wordCount.forEach((wordCount)=>{
+            wordCount.textContent=numWords.toString(10)
+        })
+
+        this.charCount.forEach((charCount)=>{
+            charCount.textContent=numChars.toString(10)
+        })
+    }
+}
+
+new Count()
